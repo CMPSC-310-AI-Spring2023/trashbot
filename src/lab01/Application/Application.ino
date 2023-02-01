b7de2c0754a1a635452a8301213bc30a97c585b6
TitusSmith33 <89529322+TitusSmith33@users.noreply.github.com>
2023-02-01 15:47:22 -0500

    Changed Code a bit

--- src/lab01/Application/Application.ino
+++ src/lab01/Application/Application.ino
@@ -4,33 +4,37 @@ Titus Smith
 Luke Barker
 */
 
-#include<servo.h>
-    Servo serv;
-int const trig = 6;
-int const echo = 5;
+#include<Servo.h>
+Servo serv;
+int const trigPin = 6;
+int const echoPin = 5;
 void setup()
 {
-pinMode(trig, OUTPUT);
-pinMode(echo, INPUT);
-    serv.attach(3);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+serv.attach(3);
 }
 void loop()
-{       int duration, distance;
-digitalWrite(trig, HIGH);
+{       
+int duration, distance;
+digitalWrite(trigPin, HIGH);
 delay(1);
-digitalWrite(trig, LOW);
+digitalWrite(trigPin, LOW);
 // Measure pulse input in echo pin
-duration = pulseIn(echo, HIGH);
+duration = pulseIn(echoPin, HIGH);
 // Distance is half duration devided by 29.1
 distance = (duration/2) / 29.1;
 // if distance less than 0.5 meter and more than 0
-if (distance <= 50 && distance >= 0) {
-  serv.write(50);
-    delay(3000);
-} else {
-
-  serv.write(160);
+if (distance <= 50 && distance >= 0)
+{
+serv.write(50);
+delay(2);
 }
+else
+{
+serv.write(160);
+}
+
 // Delay time by 60 seconds to provide a buffer
-delay(60);
-}﻿
+
+}
