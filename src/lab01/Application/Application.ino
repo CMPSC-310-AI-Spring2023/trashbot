972ed53c785ed0def742f577aab1f8bc47898bd4
jackturner83 <79563663+jackturner83@users.noreply.github.com>
2023-02-08 12:19:31 -0500

    Update Application.ino

--- src/lab01/Application/Application.ino
+++ src/lab01/Application/Application.ino
@@ -1,40 +1,30 @@
-/*
-Jack Turner
-Titus Smith
-Luke Barker
-*/
 
-#include<Servo.h>
-Servo serv;
+#include <Servo.h>
+Servo servo;
 int const trigPin = 6;
 int const echoPin = 5;
-void setup()
-{
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-serv.attach(3);
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  servo.attach(3);
 }
-void loop()
-{       
-int duration, distance;
-digitalWrite(trigPin, HIGH);
-delay(1);
-digitalWrite(trigPin, LOW);
-// Measure pulse input in echo pin
-duration = pulseIn(echoPin, HIGH);
-// Distance is half duration devided by 29.1
-distance = (duration/2) / 29.1;
-// if distance less than 0.5 meter and more than 0
-if (distance <= 50 && distance >= 0)
-{
-serv.write(50);
-delay(2);
-}
-else
-{
-serv.write(160);
-}
+void loop() {
+  int duration, distance;
+  digitalWrite(trigPin, HIGH);
+  
+  digitalWrite(trigPin, LOW);
+  // Measure the pulse input in echo pin
+  duration = pulseIn(echoPin, HIGH);
+  // Distance is half the duration devided by 29.1 (from datasheet)
+  distance = (duration / 2) / 29.1;
+  // if distance less than 0.5 meter and more than 0 (0 or less means over range)
+  if (distance <= 50 && distance >= 0) {
+    servo.write(50);
+    delay(5);
+  } else {
 
-// Delay time by 60 seconds to provide a buffer
-
+    servo.write(160);
+  }
+  
+  delay(5);
 }
